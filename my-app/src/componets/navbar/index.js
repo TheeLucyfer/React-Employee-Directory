00727cbdf8a7b1026f2531f2b68b38c6ac4c8d30
TheeLucy <62519677+TheeLucyfer@users.noreply.github.com>
2020-08-29 01:43:56 -0700

    Add a header, and wrapper

---
+++ my-app/src/componets/navbar/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./style.css";
+
+function Navbar(){
+    return(
+        <h1 className="navbar navbar-expand-lg navbar-light bg-light">Employee Directory</h1> 
+    )
+}
+export default Navbar;
